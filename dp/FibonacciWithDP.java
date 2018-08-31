778e4f71280fe533a3b52d66b578555194fc6def
“Vikas <naiyarv@gmail.com>
2018-08-31 18:38:29 +0530

    Fibonacci with dp

--- dp/FibonacciWithDP.java
+++ dp/FibonacciWithDP.java
@@ -7,9 +7,39 @@ public class FibonacciWithDP {
 
     public  static void main(String[] args) {
         int n =9;
-        System.out.println("Fibonacci number at position " + n + " is " + fibonacci(n) );
+
+        int[] result = new int[n+1];
+        fibonacciMemoization(n, result);
+
+        System.out.println("Fibonacci number at position " + n + " is " + fibonacciMemoization(n, result) );
+
     }
 
+    /**
+     * Memoization technique
+     * @param n
+     * @return
+     */
+    private static int fibonacciMemoization(int n, int[] result) {
+
+        if(result[n] == 0) {
+            if(n <= 1) {
+                result[n] = n;
+            } else {
+                result[n] = fibonacciMemoization(n-1, result) + fibonacciMemoization(n-2, result);
+            }
+
+        }
+
+        return  result[n];
+
+    }
+
+        /**
+         * Top Down apprach
+         * @param n
+         * @return
+         */
     private static int fibonacci(int n) {
 
         if(n <= 1) {
